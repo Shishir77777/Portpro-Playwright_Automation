db7a85074aa1be440a348673b8d82aee32cc503f
bibas07 <koeralabibas@gmail.com>
2025-07-08 23:59:40 +0545

    Initial project setup

---
+++ playwright.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "playwright/test";
+import * as os from "node:os";
+import { devices } from "playwright";
+
+export default defineConfig({
+  timeout: 1 * 60 * 1000,
+  retries: process.env.CI ? 2 : 1,
+  workers: 2,
+  reporter: [
+    ["line"],
+    [
+      "allure-playwright",
+      {
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
+    ],
+  ],
+  use: {
+    trace: "retry-with-trace",
+    video: "retry-with-video",
+    screenshot: "on-first-failure",
+  },
+
+  projects: [
+    {
+      name: "Desktop Chrome",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "Desktop Firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+  ],
+});
